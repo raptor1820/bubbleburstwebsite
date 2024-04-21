3032b10e218520c36f0cf85a0e44537bce3af825
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2024-04-21 15:27:16 +0530

    first comit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,124 @@
+<script>
+	import Card from './Card.svelte';
+	let url = '';
+	let data = [];
+
+	async function handleClick() {
+		await fetch('http://z808c80.139.59.37.79.sslip.io/?url=' + url).then(async (res) => {
+			let x = await res.json();
+			console.log(x);
+			data = [];
+			for (let item in x) {
+				data = [...data, x[item]];
+			}
+			console.log(data);
+		});
+	}
+</script>
+
+<svelte:window on:keydown={(e) => e.key === 'Enter' && handleClick()} />
+<div class="main-cont">
+	<h1 class="main-header">BUBBLE BURST!</h1>
+	<div class="input-cont">
+		<input type="text" bind:value={url} class="textbox" />
+		<button on:click={handleClick} class="submit-button">Go</button>
+	</div>
+	<h2 class="under-text">
+		<i>Enter the URL of any news article and press GO to see the other side of the story...</i>
+	</h2>
+	{#each data as item}
+		<Card {item} />
+	{/each}
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@600&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@600&family=Rubik+Iso&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Varela+Round&display=swap');
+	:global(body) {
+		background-color: #ffebca;
+		scrollbar-color: black transparent;
+		scrollbar-width: 10px;
+	}
+	:global(::-webkit-scrollbar) {
+		width: 10px;
+		background: transparent;
+	}
+	:global(::-webkit-scrollbar-thumb) {
+		background: black;
+		border-radius: 30px;
+	}
+	.main-header {
+		font-family: 'Press Start 2P', cursive;
+		font-size: 5vw;
+	}
+	.main-cont {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.input-cont {
+		width: 60vw;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+	.textbox {
+		background-color: #ffebca;
+		box-shadow: 3px 4px 0px 1px #000;
+		border: 2px solid #000;
+		height: 4rem;
+		width: 60rem;
+		overflow: hidden;
+		outline: none;
+		font-family: 'Varela Round', sans-serif;
+		font-size: 2rem;
+		overflow: hidden;
+	}
+	.textbox:hover,
+	.textbox:focus {
+		transform: translate(2px, 2px);
+		box-shadow: 2px 2px 0px 0px;
+	}
+	.submit-button {
+		padding: 0;
+		margin: auto;
+		background-color: #ffebca;
+		box-shadow: 3px 4px 0px 1px #000;
+		border: 2px solid #000;
+		height: 4rem;
+		width: 5rem;
+		font-family: 'Varela Round', sans-serif;
+		font-size: 2rem;
+		outline: none;
+		overflow: hidden;
+	}
+	.submit-button:hover {
+		transform: translate(2px, 2px);
+		cursor: pointer;
+		box-shadow: 2px 2px 0px 0px;
+	}
+	.under-text {
+		font-family: 'Varela Round', italic;
+		font-size: 1.2vw;
+		margin-top: 10px;
+	}
+	@media (max-width: 820px) {
+		.textbox {
+			width: 80rem;
+			height: 2.5rem;
+			font-size: 1.5rem;
+		}
+		.submit-button {
+			height: 2.9rem;
+			width: 20rem;
+			font-size: 1.5rem;
+		}
+		.under-text {
+			font-size: 3vw;
+		}
+	}
+</style>
---
+++ src/routes/Card.svelte
@@ -0,0 +1,79 @@
+<script>
+	export let item;
+</script>
+
+{#if item === 'error'}
+	<div class="error-cont">
+		<h1 class="error-text">
+			Hmm... We cannot access this URL. <br />
+			<span>Either the URL is incorrect, or our services may be down.</span>
+		</h1>
+	</div>
+{:else}
+	<div class="container">
+		<a class="title" target="_blank" href={item.link}>{item.title}</a>
+		<p class="origin">{item.displayLink}</p>
+		<!-- <p id="readtime0" class="readtime"></p> -->
+	</div>
+{/if}
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@600&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@600&family=Rubik+Iso&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Varela+Round&display=swap');
+	.origin {
+		position: absolute;
+		font-family: 'Varela Round', sans-serif;
+		bottom: 0px;
+		left: 20px;
+		font-size: 13px;
+		padding-top: 5px;
+	}
+	.container {
+		height: 75px;
+		width: 90%;
+		background-color: #ffebca;
+		/* border: 1px solid burlywood; */
+		box-shadow: 3px 4px 0px 1px #000;
+		/* border-left: 0;
+  border-right: 0; */
+		border: 2px solid #000;
+
+		padding: 12px 10px;
+		border-radius: 4px;
+		position: relative;
+		overflow: hidden;
+		margin-top: 10px;
+		margin-bottom: 10px;
+	}
+	.title {
+		font-size: 17px;
+		position: relative;
+		font-family: 'Varela Round', sans-serif;
+		top: 10px;
+		left: 10px;
+		font-weight: bold;
+		color: black;
+		text-align: left;
+		padding-top: 3px;
+		overflow-y: hidden;
+		word-wrap: break-word;
+	}
+	.error-text {
+		font-family: 'Varela Round', sans-serif;
+		font-size: 3vw;
+		margin: auto;
+	}
+	.error-cont {
+		display: flex;
+		text-align: center;
+		flex-direction: column;
+		align-items: center;
+		width: 80%;
+		padding: 19px 10px;
+		padding-top: 30px;
+		justify-content: center;
+	}
+</style>
